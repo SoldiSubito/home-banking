f2ab7adb8e2b7d9da41e8cdfb6f8a16b27db86e2
Lorenzo <dudongo55@gmail.com>
2020-06-12 16:51:25 +0200

    Praticamente nulla

--- src/main/java/soldiSubito/home_banking/UserManagement.java
+++ src/main/java/soldiSubito/home_banking/UserManagement.java
@@ -1,5 +1,7 @@
 package soldiSubito.home_banking;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class UserManagement {
@@ -9,7 +11,7 @@ public class UserManagement {
 	public static void register(
 		String name,
 		String surname,
-		Date dateOfBirth,
+		LocalDate dateOfBirth,
 		Gender gender,
 		String birthPlace,
 		String livingPlace,
@@ -17,6 +19,11 @@ public class UserManagement {
 		String phoneNumber,
 		String eMail,
 		String identityId) {
+			long age = ChronoUnit.YEARS.between(dateOfBirth, LocalDate.now());
+			if (age < 18) throw new IllegalArgumentException("Devi avere almeno 18 anni per creare un account");
+			
+		}
+
 		
 	}
 	public void modifyPhoneNumber() {
