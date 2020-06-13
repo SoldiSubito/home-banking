52320a87b48955cdb4627505d20ad230a28f66cc
yarianoffo <yari.anoffo@gmail.com>
2020-06-13 16:58:02 +0200

    13_06_2020 agg query
    Insert user + generic OK
    Sistemare Data

--- src/main/java/soldiSubito/home_banking/UserManagement.java
+++ src/main/java/soldiSubito/home_banking/UserManagement.java
@@ -16,13 +16,18 @@ import java.util.Scanner;
 
 public class UserManagement {
 
+	//POST
 	public static void login(String cf, String password) {
+		//Query SELECT * FROM users WHERE cf = cf;
 		
+		//risposta tutti i parametri per identificare l'utente (TOKEN NECESSARIO)
 	}
+	
+	//POST
 	public static void register(
 		String name,
 		String surname,
-		LocalDate dateOfBirth,
+		Date dateOfBirth,
 		Gender gender,
 		String birthPlace,
 		String livingPlace,
@@ -30,27 +35,27 @@ public class UserManagement {
 		String phoneNumber,
 		String eMail,
 		String identityId) throws SQLException {
-			long age = ChronoUnit.YEARS.between(dateOfBirth, LocalDate.now());
-			if (age < 18) throw new IllegalArgumentException("Devi avere almeno 18 anni per creare un account.");
+		//	long age = ChronoUnit.YEARS.between(dateOfBirth, Date.valueOf(date));
+		//	if (age < 18) throw new IllegalArgumentException("Devi avere almeno 18 anni per creare un account.");
 			if (name.isBlank()) throw new IllegalArgumentException("Il nome non può essere vuoto.");
 			if (surname.isBlank()) throw new IllegalArgumentException("Il cognome non può essere vuoto.");
 			if (birthPlace.isBlank()) throw new IllegalArgumentException("Il luogo di nascita non può essere vuoto.");
 			if (livingPlace.isBlank()) throw new IllegalArgumentException("La residenza non può essere vuota.");
 			//if (LocalDate.now() < dateOfBirth) throw new IllegalArgumentException("La data di nascita non può essere nel futuro.");
 			if (!isValidFiscalCode(cf)) throw new IllegalArgumentException("Il codice fiscale non è corretto");
-			if (!isValidNumeroFisso(phoneNumber.trim()) ||
+			if (!isValidNumeroFisso(phoneNumber.trim()) &&
 					!isValidNumeroMobile(phoneNumber.trim())) throw new IllegalArgumentException("Il numero di telefono non è corretto");
 			if (!isValidMail(eMail)) throw new IllegalArgumentException("L'email non è corretta");
 			//forse controllo identity Id
 			int generatedId = saveGenerics(livingPlace, eMail, phoneNumber, birthPlace);
 			
 			//usare try catch oppure aggiungere throw SQLException
-			DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");  
-			String strDate = dateFormat.format(dateOfBirth);  
-			saveUser(new String[] {name, surname, cf, "token", strDate, Integer.toString(generatedId)});
+		//	DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");  
+		//	String strDate = dateFormat.format(dateOfBirth);  
+			saveUser(new String[] {name, surname, cf, "token", Integer.toString(generatedId)}, dateOfBirth);
 		}
 
-		
+	
 		
 		
 	
@@ -84,7 +89,7 @@ public class UserManagement {
 	}
 	private static int saveGenerics(String lp, String em, String pn, String bp) throws SQLException {
 		Connection myConnection = DBConnection.connect();
-		String myQuery = "INSERT INTO generics(livingPlace, eMail, phoneNumber, birthPlace) VALUES (?,?,?,?)";
+		String myQuery = "INSERT INTO generics(living_place, email, phone_number, birth_place) VALUES (?,?,?,?)";
 		PreparedStatement preparedStatement = myConnection.prepareStatement(myQuery, Statement.RETURN_GENERATED_KEYS);
 		int generatedKey = -1;
 		try {
@@ -105,7 +110,7 @@ public class UserManagement {
 		}
 		return generatedKey;
 	}
-	private static void saveUser(String[] data) throws SQLException {
+	private static void saveUser(String[] data, Date dateOfBirth) throws SQLException {
 		Connection myConnection = DBConnection.connect();
 		
 		String myQuery = "INSERT INTO user(name, surname, fiscal_code, token, birth_date, contact, create_at, update_at)" + 
@@ -116,9 +121,10 @@ public class UserManagement {
 			preparedStatement.setString(2, data[1]);
 			preparedStatement.setString(3, data[2]);
 			preparedStatement.setString(4, data[3]);
-			Date date=Date.valueOf(data[4]); 
-			preparedStatement.setDate(5, date);
-			preparedStatement.setInt(6, Integer.parseInt(data[5]));
+			
+			//Date date=Date.valueOf(data[4]); 
+			preparedStatement.setDate(5, dateOfBirth);
+			preparedStatement.setInt(6, Integer.parseInt(data[4]));
 			preparedStatement.setTimestamp(7, Timestamp.valueOf(LocalDateTime.now()));
 			preparedStatement.setTimestamp(8,Timestamp.valueOf(LocalDateTime.now()));
 			//preparedStatement.setInt(9, Integer.parseInt(data[6]));
